39c57dec7bd4fc6cd8afda61a8e25bcd1ea62e94
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-02-21 02:20:41 +0100

    Updates

---
+++ precache-manifest.ecd34b538336eb35bcaf3ab6bfadc52d.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "a73d6dc29f0ef7b05b18",
+    "url": "/srvedca/static/css/main.a697a389.chunk.css"
+  },
+  {
+    "revision": "a73d6dc29f0ef7b05b18",
+    "url": "/srvedca/static/js/main.a73d6dc2.chunk.js"
+  },
+  {
+    "revision": "483a0879fa0e6e687539",
+    "url": "/srvedca/static/js/1.483a0879.chunk.js"
+  },
+  {
+    "revision": "82bc3df5d54b4c21a252",
+    "url": "/srvedca/static/css/2.5005b916.chunk.css"
+  },
+  {
+    "revision": "82bc3df5d54b4c21a252",
+    "url": "/srvedca/static/js/2.82bc3df5.chunk.js"
+  },
+  {
+    "revision": "8e626c0c23db0987cba2",
+    "url": "/srvedca/static/js/runtime~main.8e626c0c.js"
+  },
+  {
+    "revision": "33250d6df8d7cf3fda6f77fe3c98d7db",
+    "url": "/srvedca/static/media/ENSPY.33250d6d.jpg"
+  },
+  {
+    "revision": "1c01bf41dc1bf8f395422a36572c314b",
+    "url": "/srvedca/static/media/Logobar.1c01bf41.svg"
+  },
+  {
+    "revision": "c329d47e30c4657618bc8172ad0d469c",
+    "url": "/srvedca/static/media/Validate.c329d47e.svg"
+  },
+  {
+    "revision": "f2014a9a1dcd7a8df40bf791088f2ecf",
+    "url": "/srvedca/static/media/Sprint.f2014a9a.svg"
+  },
+  {
+    "revision": "2a92e7c41a0c443700d8afaf73ac44c4",
+    "url": "/srvedca/static/media/search.2a92e7c4.svg"
+  },
+  {
+    "revision": "358d9f307a1ed77804ed9bef8bc45f7e",
+    "url": "/srvedca/static/media/Delete.358d9f30.svg"
+  },
+  {
+    "revision": "2ca6c27d8e8fd266d3e6bb9853f897f1",
+    "url": "/srvedca/static/media/Modifier.2ca6c27d.svg"
+  },
+  {
+    "revision": "e8c9af2dcb49d8ec79a6e34049193e8b",
+    "url": "/srvedca/static/media/HomeMainTextContent.e8c9af2d.svg"
+  },
+  {
+    "revision": "c49b2a03d0979832c8d575a6b9c6e817",
+    "url": "/srvedca/static/media/BienvenuWomanSVG.c49b2a03.svg"
+  },
+  {
+    "revision": "ea4e017c91295fd719daa672c19f9eaa",
+    "url": "/srvedca/static/media/BienvenuEffectSVG.ea4e017c.svg"
+  },
+  {
+    "revision": "c6a2afd7994c016d1fedab7d8c1adac5",
+    "url": "/srvedca/static/media/EnrolStudents.c6a2afd7.png"
+  },
+  {
+    "revision": "d25bc190f72c88c19c7e4ca0d6a13740",
+    "url": "/srvedca/static/media/Class.d25bc190.png"
+  },
+  {
+    "revision": "62be20f0009610a74b37f3364260f35b",
+    "url": "/srvedca/static/media/Courses.62be20f0.png"
+  },
+  {
+    "revision": "d80cd4ae79a40a5f6437d80609715a49",
+    "url": "/srvedca/static/media/Admin.d80cd4ae.png"
+  },
+  {
+    "revision": "1e993332e25a87452950ada42cc120da",
+    "url": "/srvedca/static/media/Index2.1e993332.jpg"
+  },
+  {
+    "revision": "07bc654fdc8cdb90f430e7e7fc24ac6c",
+    "url": "/srvedca/static/media/Empreinte.07bc654f.png"
+  },
+  {
+    "revision": "00d650848651c98b4ebaf746ffdbed99",
+    "url": "/srvedca/static/media/IndexBgImage.00d65084.jpg"
+  },
+  {
+    "revision": "c672d8f0525a816fdff095d677754f5a",
+    "url": "/srvedca/static/media/Login1.c672d8f0.jpg"
+  },
+  {
+    "revision": "76c53eaa2d27bcff2d6430150df2e304",
+    "url": "/srvedca/static/media/Login2.76c53eaa.jpg"
+  },
+  {
+    "revision": "bf47be71fb9166e601e76dfc3aee6caa",
+    "url": "/srvedca/static/media/Ellipse1.bf47be71.svg"
+  },
+  {
+    "revision": "a53c81329273495d0aab325c48fcc12d",
+    "url": "/srvedca/static/media/Ellipse2.a53c8132.svg"
+  },
+  {
+    "revision": "95267af8fbb7ab0f8666f72504483373",
+    "url": "/srvedca/static/media/Ellipse3.95267af8.svg"
+  },
+  {
+    "revision": "98abb98baf6786c9daf11a139ce44a91",
+    "url": "/srvedca/static/media/Ellipse4.98abb98b.svg"
+  },
+  {
+    "revision": "b2841f53d3a039c9b08d804fd5dc4c55",
+    "url": "/srvedca/static/media/Ellipse5.b2841f53.svg"
+  },
+  {
+    "revision": "e531d908eba274541dde0ef9cff6a945",
+    "url": "/srvedca/static/media/Icon1.e531d908.svg"
+  },
+  {
+    "revision": "aa0e64f86ceba61fb368def315467e40",
+    "url": "/srvedca/static/media/Icon2.aa0e64f8.svg"
+  },
+  {
+    "revision": "ad91a92a7fde224bc7ff2ba3036b5e14",
+    "url": "/srvedca/static/media/Icon3.ad91a92a.svg"
+  },
+  {
+    "revision": "af3deb6a02a91a4d9aec97fc15a13e78",
+    "url": "/srvedca/static/media/Icon4.af3deb6a.svg"
+  },
+  {
+    "revision": "36cec1225c44af7539a45b8c2d79c3f8",
+    "url": "/srvedca/static/media/Icon7.36cec122.svg"
+  },
+  {
+    "revision": "a0e9b72e3f7498d58ab9f9f2e1caae36",
+    "url": "/srvedca/static/media/Icon6.a0e9b72e.svg"
+  },
+  {
+    "revision": "d9064184851a43fee1279ca2384e6317",
+    "url": "/srvedca/static/media/Icon5.d9064184.svg"
+  },
+  {
+    "revision": "dcf64eb730741698ccf990a74bc60f36",
+    "url": "/srvedca/index.html"
+  }
+];
