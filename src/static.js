404da4d9b4e517739502047f773120022121d205
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-05-29 17:53:26 +0100

    Changement du lien du backend

--- src/static.js
+++ src/static.js
@@ -1,4 +1,5 @@
-export const API = " https://projet-electronique-backend-production.up.railway.app/api/"
+export const API = " https://projet-electronique-backend.onrender.com/api/"
+//export const API = " https://projet-electronique-backend-production.up.railway.app/api/"
 // export const API = " http://localhost:8080/api/"
 
 export const listeSemestres = [1, 2]
